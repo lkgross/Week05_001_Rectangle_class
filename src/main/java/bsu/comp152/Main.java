5509bfec3fcaa6cf537b25ec5e82e4ed07c1fa81
lkgross <profgross@gmail.com>
2023-02-22 14:16:54 -0500

    Include more detail

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,11 +1,48 @@
 package bsu.comp152;
 
+/**
+ * Is a (container) class for the Java program we run. It contains a main method with client code (a driver program) to create and manipulate objects using methods from the (blueprint) Rectangle class.
+ */
 public class Main {
     public static void main(String[] args) {
-        Rectangle rect = new Rectangle();
+        /*
+         * Create Rectangle object.
+         * Call it rect.
+         * Do an inline initialization of a Rectangle object.
+         * We instantiate the Rectangle class.
+         * We are creating an instance of the Rectangle class.
+         */
+        /*
+         * There is a default Rectangle constructor that takes 0 arguments.
+         * However, it is no longer available after we write our own constructor in the Rectangle class.
+         */
+        // Rectangle rect = new Rectangle();
+        Rectangle rect = new Rectangle(11.71, 9.6);
+        /*
+         * Manipulate the Rectangle object rect using methods from the Rectangle class
+         *
+         * On rect, call the getLength method.
+         * Print the returned value.
+         */
         System.out.println(rect.getLength());
+        /*
+         * On rect, call the getLength method.
+         * Print the returned value.
+         */
         System.out.println(rect.getWidth());
+        /*
+         * On rect, call the setLength method.
+         */
         rect.setLength(10.2);
         System.out.println(rect.getLength());
+        rect.setWidth(2.33);
+        System.out.println(rect.getWidth());
+        /*
+         * Printing an object directly gives the memory address for the object-
+         * unless there is a toString method in the object's class, in which case the toString method gets called. (That toString method overrides the printing of the memory address.)
+         */
+        System.out.println(rect);
+
+        System.out.println(rect.toString());
     }
 }
--- src/main/java/bsu/comp152/Rectangle.java
+++ src/main/java/bsu/comp152/Rectangle.java
@@ -1,10 +1,42 @@
 package bsu.comp152;
 
+import java.util.Objects;
+
+/**
+ * A (blueprint) class for creating and manipulating
+ * Rectangle objects.
+ */
 public class Rectangle {
 
+    /*
+     * The Rectangle class has (private) "instance fields"
+     * length and width.
+     * Declare these at the top of the class.
+     */
     private double length;
     private double width;
 
+    /*
+     * A constructor has the same name as the class.
+     * A constructor is public.
+     * A constructor has no return type, not even void!
+     */
+    public Rectangle(double len, double wid){
+        length = len;
+        width = wid;
+    }
+
+    /*
+     * The Rectangle class has (public) "instance methods",
+     * e.g., getLength, getWidth, setLength.
+     * Accessors (getters) return information about the rectangle.
+     * Mutators (setters) set (modify) information associated with the rectangle.
+     */
+
+    /**
+     * A getter (accessor) in the Rectangle class
+     * @return the length of the rectangle
+     */
     public double getLength(){
         return length;
     }
@@ -13,7 +45,31 @@ public class Rectangle {
         return width;
     }
 
+    @Override
+    public String toString(){
+        return String.format("Rectangle: length %f, width %f", length, width);
+    }
+
+    @Override
+    public boolean equals(Rectangle other) {
+       // placeholder
+        // TODO: Complete this part!
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(length, width);
+    }
+
+    /**
+     * A setter (mutator) in the Rectangle class
+     */
     public void setLength(double len){
         length = len;
+    }
+
+    public void setWidth(double wid){
+        width = wid;
     }
 }
